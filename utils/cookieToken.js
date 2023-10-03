cfa9cc67cb529fcf9538a66ef381f7a33845d073
diwakar <d.7484959595@gmail.com>
2023-10-03 12:39:36 +0530

    added the cookie token

---
+++ utils/cookieToken.js
@@ -0,0 +1,16 @@
+const Cookietoken=(res,User)=>{
+
+const token=User.getjwtToken()
+const options={
+    expires:new Date(Date.now() + 3 * 60 * 60 * 1000),
+    httponly:true
+}
+
+res.status(200).cookie('cookieToken',token,options).json({
+  success:true,
+  User,
+
+})
+
+}
+module.exports=Cookietoken
