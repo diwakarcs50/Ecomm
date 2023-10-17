f969b2f9f5384da2d714b90470c054c8d045ac57
diwakar <d.7484959595@gmail.com>
2023-10-18 00:46:04 +0530

    user can now change details

--- routes/signin.js
+++ routes/signin.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const router=express.Router()
 
-const {signin,logout,forgotpassword,passwordreset,dashboard,changePassword}=require('../controllers/UserControllers')
+const {signin,logout,forgotpassword,passwordreset,dashboard,changePassword,updateDetails}=require('../controllers/UserControllers')
 const { resolveContent } = require('nodemailer/lib/shared')
 const { isLoggedIn } = require('../middlewares/users')
 
@@ -13,6 +13,7 @@ router.route("/forgotpassword").post(forgotpassword)
 router.route("/password/reset/:token").post(passwordreset)
 router.route("/dashboard").get(isLoggedIn,dashboard)
 router.route("/changepassword").post(isLoggedIn,changePassword)
+router.route("/change/dashboard").post(isLoggedIn,updateDetails)
 
 
 module.exports=router
